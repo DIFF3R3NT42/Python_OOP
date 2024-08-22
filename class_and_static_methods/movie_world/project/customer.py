47a31eb57c993097f24303f103b7f6628fe319e7
Djaner Salibryam <dfthae2019@proton.me>
2024-08-22 23:27:26 +0300

    class_and_static_methods exercises

--- class_and_static_methods/movie_world/project/customer.py
+++ class_and_static_methods/movie_world/project/customer.py
@@ -1,5 +1,13 @@
+from typing import List
+
+
 class Customer:
     def __init__(self, name: str, age: int, identification: int):
         self.name = name
         self.age = age
         self.id = identification
+        self.rented_dvds: List[DVD] = []
+
+    def __repr__(self):
+        return (f"{self.id}: {self.name} of age {self.age} has {len(self.rented_dvds)} rented"
+                f" DVD's ({', '.join(d.name for d in self.rented_dvds)})")
---
+++ class_and_static_methods/movie_world/project/dvd.py
@@ -0,0 +1,11 @@
+class DVD:
+    def __init__(self, name: str, dvd_id: int, creation_year: int, creation_month: str, age_restriction: int):
+        self.name = name
+        self.id = dvd_id
+        self.creation_year = creation_year
+        self.creation_month = creation_month
+        self.age_restriction = age_restriction
+        self.is_rented = False
+
+    def from_date(self, dvd_id: int, name: str, date: str, age_restriction: int):
+
